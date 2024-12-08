4531297dfff71a57e3ef5247592be88ca049d2db
Shaqayeq-taheri <shaqayeq.taheri@gmail.com>
2024-12-08 19:55:44 +0100

    install flowbite-react create the header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Navbar} from "flowbite-react"
+
+function Header() {
+  return (
+    <Navbar className="border-b-2 bg-slate-300">
+      this is the header of the app
+    </Navbar>
+  )
+}
+
+export default Header
