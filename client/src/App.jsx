aadd7db64f5b17a7eeb9adc01ae2ca73cb14bc0f
Shaqayeq-taheri <shaqayeq.taheri@gmail.com>
2024-12-08 17:48:52 +0100

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import SignUp from "./pages/Signup";
+import SignIn from "./pages/SignIn"
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+
 
 function App() {
-  return (
-    <div className='text-3xl'>
-      app
-    </div>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SignUp() {
+  return <div>this is the Sign up page</div>;
+}
+
+export default SignUp
