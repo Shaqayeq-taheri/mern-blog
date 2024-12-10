81aa2d604e2d11ec313177d2501426cb96fe7fa1
Shaqayeq-taheri <shaqayeq.taheri@gmail.com>
2024-12-10 16:53:21 +0100

    signing up function and check it via insomnia

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,16 @@
+import User from "../models/user.model.js"
+import bcryptjs from "bcryptjs"
+
 
 export const signup= async(req,res)=>{
-    console.log(req.body)
+   const {userName, email, password} = req.body
+   const hashedPassword = bcryptjs.hashSync(password,10)
+   const newUser = new User({
+    userName,
+    email,
+    password:hashedPassword
+   })
+
+   await newUser.save()
+   res.json('sign up successfull')
 }
