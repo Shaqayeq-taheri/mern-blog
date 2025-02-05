9a4b1e79319863a0fa7a55c797038cec8f13462c
Shaqayeq-taheri <shaqayeq.taheri@gmail.com>
2025-02-05 18:28:02 +0100

    complete the get all posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -37,3 +37,44 @@ export const createPost = async (req, res) => {
        })
   }
 };
+
+
+export const getAllPosts = async (req, res)=>{
+    try {
+
+        const startIndex = parseInt(req.query.startIndex) || 0
+        const limit = parseInt(req.query.limit)|| 9
+        const sortDirection = req.query.order === 'asc' ? 1 : -1 //1 ascending , -1 descending
+        const posts = await Post.find({
+            ...(req.query.userId && {userId: req.query.userId}),
+            ...(req.query.category && {category: req.query.category}),
+            ...(req.query.slug && {slug:req.query.slug}),
+            ...(req.query.postId && {_id: req.query.postId}),
+            ...(req.query.searchTerm && {
+                $or:[
+                    {title: {$regex: req.query.searchTerm, $options: 'i'}},
+                    {content: {$regex: req.query.searchTerm, $options: 'i'}}
+                ],
+            })
+    }).sort({updatedAt: sortDirection}).skip(startIndex).limit(limit)
+
+    const totalPosts = await Post.countDocuments()
+    const now = new Date()
+    const oneMonthAgo = new Date(
+        now.getFullYear(),
+        now.getMonth()- 1,
+        now.getDate()
+    )
+        
+
+    const lastMonthsPosts = await Post.countDocuments({
+        createdAt:{$gte:oneMonthAgo}
+    })
+
+
+    res.status(StatusCodes.OK).json({posts, totalPosts, lastMonthsPosts})
+    } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: "Internal Server Error",
+    });    }
+}
--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import {createPost} from '../controllers/post.controller.js'
+import {createPost, getAllPosts} from '../controllers/post.controller.js'
 
 
 const router = express.Router()
 
 router.post('/create-post', verifyToken, createPost)
+router.get('/allPosts', getAllPosts)
 
 export default router
