705c26114fbe3c7483bbd91abc0516c363c2eb5f
Shaqayeq-taheri <shaqayeq.taheri@gmail.com>
2025-02-14 14:15:34 +0100

    dashboard page bugs fixed

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -28,7 +28,7 @@ function Dashboard() {
             <div className="flex flex-1 justify-center">
                 <div
                     className={`w-full ${
-                        tab === "posts" || "users" ? "max-w-5xl" : "max-w-lg"
+                        tab === "posts" || tab==="users" ? "max-w-5xl" : "max-w-lg"
                     }`}
                 >
                     {tab === "profile" && <DashProfile />}
