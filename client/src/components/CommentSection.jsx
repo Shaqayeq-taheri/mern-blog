29084ac57709ae7e72128e793b4d3318ba24f22e
Shaqayeq-taheri <shaqayeq.taheri@gmail.com>
2025-02-18 14:15:41 +0100

    connecting comment UI to the api endpoint for posting a comment

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,4 +1,4 @@
-import { Button, Textarea } from "flowbite-react";
+import { Alert, Button, Textarea } from "flowbite-react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -6,7 +6,29 @@ import { Link } from "react-router-dom";
 function CommentSection({ postId }) {
     const { currentUser } = useSelector((state) => state.user);
     const[comment, setComment]= useState('')
+    const [commentError, setCommentError]= useState(null)
     const handleSubmit= async(e)=>{
+        e.preventDefault()
+        if(comment.length > 200){
+            return
+        }
+        try {
+            
+            const res = await fetch("/api/comment/create-comment",{
+                method:'POST',
+                headers:{'Content-Type':'application/json'},
+                body: JSON.stringify({content: comment, postId, userId:currentUser._id})
+            });
+            const data= await res.json()
+            if(res.ok){
+                setComment('')       //clear comment section
+                setCommentError(null) 
+            }
+        } catch (error) {
+            console.log(error.message)
+            setCommentError(error.message)
+            
+        }
 
     }
     return (
@@ -40,18 +62,34 @@ function CommentSection({ postId }) {
             )}
             {/* comment section */}
             {currentUser && (
-                <form onSubmit={handleSubmit} className=" border border-teal-500 rounded-md p-3">
+                <form
+                    onSubmit={handleSubmit}
+                    className=" border border-teal-500 rounded-md p-3"
+                >
                     <Textarea
                         placeholder="Add a comment..."
                         rows="3"
                         maxLength="200"
-                        onChange={(e)=>setComment(e.target.value)}
+                        onChange={(e) => setComment(e.target.value)}
                         value={comment}
                     />
                     <div className="flex justify-center items-center mt-5">
-                        <p className="text-gray-500 text-sm">{200 - comment.length} characters remaining</p>
+                        <p className="text-gray-500 text-sm">
+                            {200 - comment.length} characters remaining
+                        </p>
                     </div>
-                    <Button outline type='submit'gradientDuoTone="purpleToBlue">Submit</Button>
+                    <Button
+                        outline
+                        type="submit"
+                        gradientDuoTone="purpleToBlue"
+                    >
+                        Submit
+                    </Button>
+                    {commentError && (
+                        <Alert color="failure" className="mt-5">
+                            {commentError}
+                        </Alert>
+                    )}
                 </form>
             )}
         </div>
