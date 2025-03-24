690195ceb035c01e62ab646fc3c7c3fcfc8f9295
Shaqayeq-taheri <shaqayeq.taheri@gmail.com>
2025-03-24 22:15:02 +0100

    getting data from url and saving them in a state as an object

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import { TextInput } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function Search() {
-  return (
-    <div>
-      search page
-    </div>
-  )
+    const [sidebarData, setSidebarData] = useState({
+        searchTerm: "",
+        sort: "desc",
+        category: "uncategorized",
+    });
+    const [post, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [showmore, setShowMore] = useState(false);
+
+    const location = useLocation();
+
+    console.log(sidebarData)
+
+    useEffect(() => {
+        //the things that we want to get from url:
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFormUrl = urlParams.get("searchTerm");
+        const sortFormUrl = urlParams.get("sort");
+        const categoryFormUrl = urlParams.get("category");
+
+        if (searchTermFormUrl || sortFormUrl || categoryFormUrl) {
+            setSidebarData({
+                ...sidebarData,
+                searchTerm: searchTermFormUrl,
+                sort: sortFormUrl,
+                category: categoryFormUrl,
+            });
+        }
+    }, [location.search]);
+
+    return (
+        <div>
+            <div className="">
+                <form>
+                    <label>Search here...</label>
+                    <TextInput
+                        placeholder="search..."
+                        id="searchTerm"
+                        type="text"
+                    />
+                </form>
+            </div>
+        </div>
+    );
 }
 
-export default Search
+export default Search;
